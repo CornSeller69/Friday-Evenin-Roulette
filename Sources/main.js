fc59c971b63c36220c096fe9ddf6d127012b6fbc
CornSeller <166633175+CornSeller69@users.noreply.github.com>
2024-10-28 01:23:28 +0100

    added sources and materials
    pain.

---
+++ Sources/main.js
@@ -0,0 +1,159 @@
+// Usuń po wprowadzeniu już tych funkcji
+
+function startMP() {alert("Opcja wciąż w produkcji");}
+
+// -------------------------------------
+
+function showAchieves() {
+    document.getElementById("achievy").style.visibility = 'visible';
+    document.getElementById("achievy").style.opacity = 1;
+}
+function closeAchieves() {
+    document.getElementById("achievy").style.opacity = 0;
+    setTimeout(function() {
+        achievy.style.visibility = 'hidden';
+      }, 300);
+}
+
+function showCredits() {
+    document.getElementById("credits").style.visibility = 'visible';
+    document.getElementById("credits").style.opacity = 1;
+}
+function closeCredits() {
+    document.getElementById("credits").style.opacity = 0;
+    setTimeout(function() {
+        credits.style.visibility = 'hidden';
+      }, 300);
+}
+
+function startCampaign() {
+    document.body.style.backgroundColor = 'white'; // bo powinno być ale na początku dałem black by sie nie wyróżniało przed załadowaniem css
+    sfx_newgame.play();
+    document.getElementById("startGry").style.pointerEvents = 'none';
+    document.getElementById("startMultigry").style.pointerEvents = 'none';
+    document.getElementById("creditsy").style.pointerEvents = 'none';
+    document.getElementById("achievements").style.pointerEvents = 'none';
+    document.getElementById("main-menu").style.opacity = 0;
+    setTimeout(function() {
+        mus_intro.play();
+        document.getElementById("pic1").style.visibility = 'visible';
+        document.getElementById("pic1").style.opacity = 0.7;
+        document.getElementById("text1").style.visibility = 'visible';
+        document.getElementById("text1").style.opacity = 0.7;
+        setTimeout(function() {
+            addEventListener("click", function a() {
+                document.getElementById("text1").style.opacity = 0;
+                document.getElementById("pic1").style.opacity = 0;
+                setTimeout(function() {
+                    document.getElementById("text1").style.visibility = 'hidden';
+                    document.getElementById("pic1").style.visibility = 'hidden';
+                }, 700)
+            document.getElementById("pic2").style.visibility = 'visible';
+            document.getElementById("pic2").style.opacity = 0.7;
+            document.getElementById("text2").style.visibility = 'visible';
+            document.getElementById("text2").style.opacity = 0.7;
+            setTimeout(function() {
+                addEventListener("click", function b() {
+                removeEventListener("click", a);
+                document.getElementById("text2").style.opacity = 0;
+                document.getElementById("pic2").style.opacity = 0;
+                setTimeout(function() {
+                    document.getElementById("text2").style.visibility = 'hidden';
+                    document.getElementById("pic2").style.visibility = 'hidden';
+                    addEventListener("click", function c() {
+                        removeEventListener("click", b);
+                        removeEventListener("click", c);
+                        document.getElementById("text3").style.opacity = 0;
+                        document.getElementById("pic3").style.opacity = 0;
+                        setTimeout(function() {
+                            document.getElementById("text3").style.visibility = 'hidden';
+                            document.getElementById("pic3").style.visibility = 'hidden';
+                            addEventListener("click", endIntro);
+                        }, 700)
+                    document.getElementById("text4").style.visibility = 'visible';
+                    document.getElementById("text4").style.opacity = 0.7;
+                    })
+                }, 700)
+            document.getElementById("pic3").style.visibility = 'visible';
+            document.getElementById("pic3").style.opacity = 0.7;
+            document.getElementById("text3").style.visibility = 'visible';
+            document.getElementById("text3").style.opacity = 0.7;
+                },700)
+            })})
+        },1000)
+    }, 6000)
+    
+}
+
+function endIntro() {
+    for (let i = 10; i > 0; i--) {
+        setTimeout(function() {
+            mus_intro.volume = Math.max(0, mus_intro.volume - 0.1);
+        }, i * 400); // Multiplied by i to stagger the timeouts
+    }      
+    removeEventListener("click", endIntro);
+    document.getElementById("intro").style.opacity = 0;
+    setTimeout(function() {
+        document.getElementById("intro").style.visibility = 'hidden';
+        gameBegin();}, 5000)
+}
+
+function gameBegin() {
+    if (!hallRepeatBlock) {
+    mus_party.play();
+    mus1loop = setInterval(function loopMus() {
+        mus_party.currentTime = 0;
+        mus_party.play();
+    },43800)
+    document.getElementById("partyhall").style.visibility = 'visible';
+    document.getElementById("partyhall").style.opacity = 1;
+    document.body.style.backgroundColor = 'black';
+    changeHallInterval = setInterval(function changehall() {
+        switch (currentHall) {
+            case 1:
+                Hall = 'url(Materials/partyhall1.png)';
+                Noor = 'sepia(1) saturate(10) hue-rotate(270deg) brightness(1.1)';
+                currentHall++;
+                break;
+            case 2:
+                Hall = 'url(Materials/partyhall2.png)';
+                Noor = 'sepia(1) saturate(10) hue-rotate(190deg) brightness(1.1)';
+                currentHall++;
+                break;
+            case 3:
+                Hall = 'url(Materials/partyhall3.png)';
+                Noor = 'sepia(1) saturate(10) hue-rotate(20deg) brightness(1.2)';
+                currentHall++;
+                break;
+            case 4:
+                Hall = 'url(Materials/partyhall4.png)';
+                Noor = 'sepia(1) saturate(10) hue-rotate(90deg) brightness(1.1)';
+                currentHall = 1;
+                break;
+        }
+        document.getElementById("partyhall").style.backgroundImage = Hall;
+        document.getElementById("dealerDoor").style.filter = Noor;
+    },350)}
+}
+
+function enterDealer() {
+    hallRepeatBlock = true;
+    clearInterval(changeHallInterval);
+    sfx_standup.play();
+    clearInterval(mus1loop);
+    mus_party.pause();
+    mus_stage1.play();
+    mus2loop = setInterval(function loopMus() {
+        mus_stage1.currentTime = 0;
+        mus_stage1.play();
+    },43800)
+    document.getElementById("partyhall").style.opacity = 0;
+    setTimeout(function() {document.getElementById("partyhall").style.visibility = 'hidden'},200)
+    document.getElementById("dealerRoom").style.visibility = 'visible';
+    document.getElementById("dealerRoom").style.opacity = 1;
+    setTimeout(dealerAsk(),950);
+}
+
+function dealerAsk() {
+    document.getElementById("dealer_release").style.visibility = 'visible';
+}
---
+++ Sources/values.js
@@ -0,0 +1,39 @@
+// Dźwięki:
+let mus_intro = new Audio('Materials/Music/introduction.mp3');
+let mus_party = new Audio('Materials/Music/techno.mp3');
+let mus_stage1 = new Audio('Materials/Music/techno_muffled.mp3');
+let mus_stage2 = new Audio('Materials/Music/everyload.mp3');
+let mus_stage3 = new Audio('Materials/Music/tense.mp3');
+let mus_win = new Audio('Materials/Music/win.mp3');
+let mus_lose = new Audio('Materials/lose.mp3');
+let mus_death = new Audio('Materials/Music/hurt.mp3');
+let sfx_newgame = new Audio('Materials/Sounds/newgame.mp3')
+let sfx_reload = new Audio('Materials/Sounds/revolver-spin.mp3');
+let sfx_shoot = new Audio('Materials/Sounds/shotty.mp3');
+let sfx_shootCut = new Audio('Materials/Sounds/shotty-cut.mp3');
+let sfx_click = new Audio('Materials/Sounds/dryfire.mp3');
+let sfx_revive = new Audio('Materials/Sounds/defibrilator.mp3');
+let sfx_standup = new Audio('Materials/Sounds/chair.mp3');
+
+// Dane:
+let liveChance; // szansa na żywy pocisk, sprawdza ile pozostało strzałów
+let bulletsLeft; // sprawdza ile pocisków zostało
+let bulletType; // sprawdza czy pocisk jest 'blank' czy 'live'
+let bulletsFired = 0; // dla podsumowania, sprawdza ile wystrzelono pocisków podczas gry
+let moneyEarned = 17000; // podsumowanie finalnych pieniędzy, różne wartości dodają punkty
+let newGame = true; // sprawdza czy gracz już przegrał wcześniej
+let name; // nazwa gracza - singleplayer
+let name1; // nazwa gracza 1 - multiplayer
+let name2; // nazwa gracza 2 - multiplayer
+let yourTurn = true; // sprawdza czyja tura, zawsze grę zaczyna gracz
+let round = 1; // obecna runda
+let playerHP = 3; // zdrowie gracza
+let dealerHP = 3; // zdrowie krupiera
+let helperSavesYou = true; // sprawdzanie do zakończenia gry, wartość ulega zmianie na false przy trzeciej rundzie lub gdy newGame = false;
+let currentHall = 1; // checks which color should partyhall become before the roulette room
+let Hall = document.getElementById("partyhall").style.backgroundImage; // Do zmiany kolorków w partyhallu (w chuj zepsute!!!!)
+let Noor = document.getElementById("dealerDoor").style.filter; // Zmiana kolorków drzwi w partyhallu
+let changeHallInterval; // Usuwanie kolorkowej hali. Zrobione w sposób obejścia bo [w momencie pisania tego] chce to na szybko zrobić bo chce mi sie spać
+let mus1loop; // j.w.
+let mus2loop; // j.w.
+let hallRepeatBlock = false; // podobnie co do j.w.
